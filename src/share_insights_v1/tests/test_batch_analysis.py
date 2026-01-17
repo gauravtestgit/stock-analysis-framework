8f3a595a332fdcf45853525a02c813838a87c5ed
gauravtestgit <gaurav.pro.id@gmail.com>
2026-01-18 05:59:55 +1300

    move thesis generator buttons to fragments

--- src/share_insights_v1/tests/test_batch_analysis.py
+++ src/share_insights_v1/tests/test_batch_analysis.py
@@ -14,7 +14,7 @@ def test_batch_analysis():
     batch_service = BatchAnalysisService(save_to_db=True, enable_detailed_news_analysis=False)
     
     # Define paths
-    filename = "nzx"
+    filename = "nasdaq"
     input_csv_dir = "./src/share_insights_v1/resources/stock_dump/"  # Assuming this exists in the root directory
     output_csv_dir = "./src/share_insights_v1/resources/stock_analyses/"
     date = datetime.now().strftime("%Y%m%d%H%M%S")
